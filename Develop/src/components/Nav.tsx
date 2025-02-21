7384b24eb86f256d4840eece4c64d73916603187
alyssadailey <alyssadailey28@gmail.com>
2025-02-21 17:34:03 -0500

    added nav links

--- Develop/src/components/Nav.tsx
+++ Develop/src/components/Nav.tsx
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Nav = () => {
   // TODO: Add necessary code to display the navigation bar and link between the pages
   return (
-    <div>Nav</div>
+    <div>
+      <Link className="home-nav" to="/">Home</Link>
+      <Link className="potential-candidates-nav" to="/SavedCandidates">Potential Candidates</Link>
+    </div>
   )
 };
 
